18bd34d7b80c45913975df24853a70d298e34dd2
Dennis Gardner <dennis.gardner@colorado.edu>
2017-03-07 11:50:16 -0500

    resized to 512x512 px and subplot

--- SharpenImage.m
+++ SharpenImage.m
@@ -2,15 +2,22 @@ function [ ] = SharpenImage( )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+% name of the image
+imageName = 'car-370695.jpg';
+
 % read in the image
-I = imread('car-370695.jpg');
+I = imread(imageName);
 
 % convert image to gray scale
 I = rgb2gray(I);
 
+% re-zsize the image
+I = imresize(I,[512,512]);
+
 figure('color','w');
+subplot(121);
 imagesc(I);
-axis off;
+axis off; axis image;
 colormap('gray');
 
 end
