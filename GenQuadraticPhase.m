c2b50e94f8205d3ee0b87d6d79aa5dc419f829d9
Dennis Gardner <dennis.gardner@colorado.edu>
2017-03-09 08:54:01 -0500

    initial setup of quadratic phase generation function

---
+++ GenQuadraticPhase.m
@@ -0,0 +1,39 @@
+function quadPhase = GenQuadraticPhase(A)
+%GENQUADRATICPHASE - apply phase array in Fourier plane to maximize sharpness
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Author: Dennis F. Gardner
+% Work address
+% email: dennis.gardner@colorado.edu
+% Website: http://www.dennisfgardner.com
+% March 2017; Last revision: 7-March-2017
+
+
+
+
+
+end
