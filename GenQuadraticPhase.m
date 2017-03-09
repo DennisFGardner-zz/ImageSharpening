e5353edd353b78b9bf023f9bf94b03e0eb4a1f30
Dennis Gardner <dennis.gardner@colorado.edu>
2017-03-09 09:05:43 -0500

    working version of script

--- GenQuadraticPhase.m
+++ GenQuadraticPhase.m
@@ -1,4 +1,4 @@
-function quadPhase = GenQuadraticPhase(A)
+function quadPhase = GenQuadraticPhase(A, maxPhase)
 %GENQUADRATICPHASE - apply phase array in Fourier plane to maximize sharpness
 %Optional file header info (to give more details about the function than in the H1 line)
 %Optional file header info (to give more details about the function than in the H1 line)
@@ -7,7 +7,7 @@ function quadPhase = GenQuadraticPhase(A)
 % Syntax:  [output1,output2] = function_name(input1,input2,input3)
 %
 % Inputs:
-%    input1 - Description
+%    A - input array to determine size of generated array
 %    input2 - Description
 %    input3 - Description
 %
@@ -24,14 +24,38 @@ function quadPhase = GenQuadraticPhase(A)
 % Subfunctions: none
 % MAT-files required: none
 %
-% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+% See also: SHARPENIMAGE,  OTHER_FUNCTION_NAME2
 
 % Author: Dennis F. Gardner
 % Work address
 % email: dennis.gardner@colorado.edu
 % Website: http://www.dennisfgardner.com
-% March 2017; Last revision: 7-March-2017
+% March 2017; Last revision: 09-March-2017
 
+[M,N] = size(A); 
+
+x = -N/2:1:N/2-1;
+y = -M/2:1:M/2-1;
+
+[X, Y] = meshgrid(x,y);
+
+
+quadPhase = X.*X + Y.*Y;
+
+quadPhase = quadPhase./max(quadPhase(:));
+
+quadPhase = quadPhase.*maxPhase;
+
+figure('color','w');
+subplot(121);
+imagesc(quadPhase);
+axis image
+title('unwrapped')
+
+subplot(122);
+imagesc(angle(exp(1i*quadPhase)));
+axis image;
+title('wrapped');
 
 
 
